ac62b46cb647e88a46030884d787028f3eb62cc0
gashug <geoffrey.gachuiri98@gmail.com>
2024-11-28 16:00:03 +0300

    added dark mode functionality

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -3,7 +3,7 @@ import { Logo } from "./logo";
 
 export const Footer = () => {
   return (
-    <div className="flex items-center w-full p-6 bg-background z-50">
+    <div className="flex items-center w-full p-6 bg-background dark:bg-[#191919] z-50">
       <Logo />
       <div className="md:ml-auto w-full justify-between md:justify-end flex items-center gap-x text-muted-foreground">
         <Button variant="ghost" size="sm">
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -36,11 +37,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="sytem"
+          enableSystem
+          disableTransitionOnChange
+          storageKey="jotion-theme"
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
